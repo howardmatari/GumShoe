af4066279183540abda37b574367051497abb680
Jorge Sepulveda <jrg.sepulveda@gmail.com>
2020-10-10 13:59:13 -0400

    update table

--- models/contacts.js
+++ models/contacts.js
@@ -29,7 +29,7 @@ module.exports = function(sequelize, DataTypes) {
         allowNull: false
       },
       notes: {
-        type: DataTypes.STRING,
+        type: DataTypes.TEXT,
         allowNull: false
       },
     });
